1f2aec0b15304b9a4e0f87dccfe56cab91f34fee
Mckane10 <kanesean@hotmail.com>
2019-03-14 12:40:47 +0100

    Add files via upload
    Le formulaire d'inscription de l'organisateur

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,5 +1,10 @@
-import React, {Component} from 'react';
-import './Register.css'
+import React, { Component } from "react";
+import {Button, FormGroup, FormControl, ButtonGroup, Form, Col, InputGroup, Row} from "react-bootstrap";
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux';
+import "./Register.css";
+
+import {userActions} from "../../_actions";
 
 class Register extends Component{
 
@@ -18,8 +23,79 @@ class Register extends Component{
 
     render(){
         return(
-            <div>
-                <h1>TEST</h1>
+            <div className="Register">
+            <Form
+        noValidate
+        //validated={validated}
+        onSubmit={e => this.handleSubmit(e)}
+      >
+      <h3>Informations personnelles : Organisateur</h3>
+        <Form.Row>
+          <Form.Group as={Col} md="4" controlId="validationCustom01">
+            <Form.Label>Prénom *</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Prénom"
+
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationCustom02">
+            <Form.Label>Nom *</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Nom"
+
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+            <Form.Label>Username *</Form.Label>
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+              </InputGroup.Prepend>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                aria-describedby="inputGroupPrepend"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please choose a username.
+              </Form.Control.Feedback>
+            </InputGroup>
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Row>
+          <Form.Group as={Col} md="4" controlId="formBasicEmail">
+            <Form.Label>Email  *</Form.Label>
+            <Form.Control type="email" placeholder="nano@nano.com" />
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="formBasicPassword">
+            <Form.Label>Mot de passe *</Form.Label>
+            <Form.Control type="password" placeholder="Mot de passe" />
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="formBasicPassword">
+            <Form.Label>Confirmer le mot de passe *</Form.Label>
+            <Form.Control type="password" placeholder="Confimer votre mot de passe." />
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Group>
+          <Form.Check
+            required
+            label="Accepter les conditions générales pour valider"
+            feedback="Vous devez être d'accord avant de vous soumettre."
+          />
+        </Form.Group>
+
+        <Button type="submit">Valider</Button>
+      </Form>
+
             </div>
         )
     }
