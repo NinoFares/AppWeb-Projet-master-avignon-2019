6c26d3676b329dc8ee80ec3de4a312db98803582
Mckane10 <kanesean@hotmail.com>
2019-05-12 15:16:56 +0200

    Commit du 12/05

--- client/src/components/ListUsersConference/ListUsersConference.js
+++ client/src/components/ListUsersConference/ListUsersConference.js
@@ -42,7 +42,7 @@ export class ListUsersConference extends Component{
                 },
                 {
                     Header: 'Session',
-                    accessor: 'idSession'
+                    accessor: 'title'
                 },
             ]
         }
